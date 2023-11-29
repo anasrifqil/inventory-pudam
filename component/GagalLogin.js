2ae8b85f306d49efbc64dd539d7c30badedced5e
Anas_Rifqil <91422087+anasrifqil@users.noreply.github.com>
2023-11-29 13:43:26 +0700

    Delete component/GagalLogin.js

--- component/GagalLogin.js
+++
@@ -1,8 +0,0 @@
-export default function GagalLogin(){
-    return (
-        <div className="gagal">
-            <h1>Upss..........</h1>
-            <p>Anda Belum Login...</p>
-        </div>
-    )
-}
